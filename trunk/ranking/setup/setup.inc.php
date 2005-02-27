e0433e36e11c886a5b30445c0b6bd0e71994ec6e
ralf <ralf@87f23f9b-da0a-4d64-8e9d-2088ad7f682b>
2005-02-27 21:33:16 +0000

    added 1.0.1 dependency and hooks for sidebox-menu, prefs and admin

--- trunk/ranking/setup/setup.inc.php
+++ trunk/ranking/setup/setup.inc.php
@@ -14,22 +14,21 @@
 
 	$setup_info['ranking']['name']      = 'ranking';
 	$setup_info['ranking']['version']   = '0.9.13.001';
-	$setup_info['ranking']['app_order'] = 5;
+	$setup_info['ranking']['app_order'] = 1;
 	$setup_info['ranking']['tables']    = array('rang.Wettkaempfe','rang.Serien','rang.Gruppen','rang.Personen','rang.Results','rang.RangListenSysteme','rang.PktSysteme','rang.Feldfaktoren','rang.Gruppen2Personen','rang.PktSystemPkte');
 	$setup_info['ranking']['enable']    = 1;
 
 	/* The hooks this app includes, needed for hooks registration */
-	//$setup_info['ranking']['hooks'][] = 'preferences';
-	//$setup_info['ranking']['hooks'][] = 'admin';
-	//$setup_info['ranking']['hooks'][] = 'about';
+	$setup_info['ranking']['hooks']['preferences'] = 'ranking.admin_prefs_sidebox_hooks.all_hooks';
+	$setup_info['ranking']['hooks']['admin'] = 'ranking.admin_prefs_sidebox_hooks.all_hooks';
+	$setup_info['ranking']['hooks']['sidebox_menu'] = 'ranking.admin_prefs_sidebox_hooks.all_hooks';
 
 	/* Dependacies for this app to work */
 	$setup_info['ranking']['depends'][] = array(
 		 'appname' => 'phpgwapi',
 		 'versions' => Array('0.9.13','0.9.14','0.9.15','1.0.0','1.0.1')
 	);
-        $setup_info['ranking']['depends'][] = array(   // this is only necessary as long the etemplate-class is not in the api
-                 'appname' => 'etemplate',
-                 'versions' => Array('0.9.13','0.9.14','0.9.15','1.0.0')
-        );
-?>
+    $setup_info['ranking']['depends'][] = array(   // this is only necessary as long the etemplate-class is not in the api
+             'appname' => 'etemplate',
+             'versions' => Array('0.9.13','0.9.14','0.9.15','1.0.0','1.0.1')
+    );
