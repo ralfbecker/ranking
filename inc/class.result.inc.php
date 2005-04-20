a012260ae1eb9b8dbd261bed81ae15ab063d4aba
Ralf Becker <rb@stylite.de>
2005-04-20 17:18:22 +0000

    Ergebnisse und letzten Wettkampf bei Athleten anzeigen

--- inc/class.result.inc.php
+++ inc/class.result.inc.php
@@ -1,173 +1,167 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare - digital ROCK Rankings - Result Object                       *
-	* http://www.egroupware.org, http://www.digitalROCK.de                     *
-	* Written and (c) by Ralf Becker <RalfBecker@outdoor-training.de>          *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+/**************************************************************************\
+* eGroupWare - digital ROCK Rankings - Result Object                       *
+* http://www.egroupware.org, http://www.digitalROCK.de                     *
+* Written and (c) by Ralf Becker <RalfBecker@outdoor-training.de>          *
+* --------------------------------------------                             *
+*  This program is free software; you can redistribute it and/or modify it *
+*  under the terms of the GNU General Public License as published by the   *
+*  Free Software Foundation; either version 2 of the License, or (at your  *
+*  option) any later version.                                              *
+\**************************************************************************/
 
-	/* $Id$ */
+/* $Id$ */
 
-require_once(PHPGW_INCLUDE_ROOT . '/etemplate/inc/class.so_sql.inc.php');
+require_once(EGW_INCLUDE_ROOT . '/etemplate/inc/class.so_sql.inc.php');
 
-/*!
-@class result
-@abstract result object
-*/
-class competition extends so_sql
+/**
+ * result object
+ */
+class result extends so_sql
 {
-	/* var $public_functions; /* = array(
-		'init'	=> True,
-		'read'	=> True,
-		'save'	=> True,
-		'delete'	=> True,
-		'search'	=> True,
-	) */	// set in so_sql
-/* set by so_sql('ranking','rang.Wettkaempfe'):
-	var $table_name = 'rang.Wettkaempfe';
-	var $autoinc_id = 'WetId';
-	var $db_key_cols = array('WetId' => 'WetId');
-	var $db_data_cols = array(
-		'rkey' => 'rkey', 'name' => 'name', 'dru_bez' => 'dru_bez', 'datum' => 'datum',
-		'pkte' => 'pkte', 'pkt_bis' => 'pkt_bis', 'feld_pkte' => 'feld_pkte', 'feld_bis' => 'feld_bis',
-		'faktor' => 'faktor', 'serie' => 'serie', 'open' => 'open','nation' => 'nation',
-		'gruppen' => 'gruppen', 'homepage' => 'homepage'
-	);
-*/
 	var $non_db_cols = array(	// fields in data, not (direct) saved to the db
 	);
-	var $per_table_name,$grp_table_name,$comp_table_name,$ff_table_name;
+	var $result_table  = 'Results';
+	var $athlete_table = 'Personen';
+	var $cat_table     = 'Gruppen';
+	var $comp_table    = 'Wettkaempfe';
+	var $ff_table      = 'Feldfaktoren';
+	var $charset,$source_charset;
 
-	/*!
-	@function result
-	@abstract result of the competition class
-	@param $WetId,$GrpId,$PerId as for read
-	*/
-	function result($WetId=0,$GrpId=0,$PerId=0)
+	/**
+	 * constructor of the competition class
+	 */
+	function result($source_charset='',$db=null,$vfs_pdf_dir='')
 	{
 		//$this->debug = 1;
-		$this->so_sql('ranking','rang.Results');	// call constructor of extending class
-
-		$this->per_table_name  = 'rang.Personen';
-		$this->cat_table_name  = 'rang.Gruppen';
-		$this->comp_table_name = 'rang.Wettkaempfe';
-		$this->ff_table_name   = 'rang.Feldfaktoren';
-
-		//$this->public_functions += array();
-
-		if ($WetId || $PerId)
-			$this->read($WetId,$GrpId,$PerId);
+		$this->so_sql('ranking',$this->result_table,$db);	// call constructor of extended class
+		
+		if ($source_charset) $this->source_charset = $source_charset;
+		
+		$this->charset = $GLOBALS['phpgw']->translation->charset();
 	}
 
-	/*!
-	@function db2data
-	@abstract changes the data from the db-format to our work-format
-	@param $data if given works on that array and returns result, else works on internal data-array
-	*/
-	function db2data($data=0)
+	/**
+	 * reads row matched by keys and puts all cols in the data array
+	 *
+	 * a) only WetId: List of cats with results for the given comp.
+	 * b) WetId and GrpId: List of Athlets = result or startlist
+	 * c) WetId, GrpId and PerId: result of a single athlet
+	 * d) only PerId: List of results of a person
+	 *
+	 * @param array $keys array with keys in form internalName => value, may be a scalar value if only one key
+	 * @param string/array $extra_cols string or array of strings to be added to the SELECT, eg. "count(*) as num"
+	 * @param string/boolean $join=true true for the default join or sql to do a join, added as is after the table-name, eg. ", table2 WHERE x=y" or 
+	 * @param string $order='' order clause or '' for the default order depending on the keys given
+	 * @return array/boolean data if row could be retrived else False
+	 */
+	function &read($keys,$extra_cols='',$join=true,$order='')
 	{
-		if ($intern = !is_array($data))
-			$data = $this->data;
+		if ($order && !preg_match('/^[a-z_, ]+$/i',$order)) $order = '';
 
+		$filter = $keys;
+		foreach(array('WetId','GrpId','PerId') as $key)
+		{
+			if ((int) $keys[$key] > 0)
+			{
+				$this->$key = (int) $keys[$key];
+			}
+			else
+			{
+				unset($keys[$key]);
+			}
+			unset($filter[$key]);
+		}
+		if ($this->WetId && $this->GrpId && !$this->PerId)	// read complete result of comp. WetId for cat GrpId
+		{
+			if ($join === true)
+			{
+				$join = ",$this->athlete_table WHERE $this->result_table.PerId=$this->athlete_table.PerId";
+				$extra_cols = 'nachname,vorname,nation,geb_date';
+			}
+			return $this->search($keys,false,$order ? $order : 'platz,nachname,vorname',$extra_cols,'',false,'AND',false,$filter,$join);
+		}
+		elseif (!$this->WetId && !$this->GrpId && $this->PerId)	// read list comps of one athlet
+		{
+			if ($join === true)
+			{
+				$join = ",$this->cat_table,$this->comp_table WHERE $this->result_table.GrpId=$this->cat_table.GrpId AND $this->result_table.WetId=$this->comp_table.WetId";
+				$extra_cols = "$this->comp_table.name AS name,$this->comp_table.rkey AS rkey,$this->cat_table.name AS cat_name,$this->cat_table.rkey AS cat_rkey";
+			}
+			return $this->search($keys,false,$order ? $order : $this->result_table.'.datum DESC,platz',$extra_cols,'',false,'AND',false,$filter,$join);
+		}
+		elseif ($this->WetId && !$this->GrpId)	// read cat-list of competition
+		{
+			if ($join === true)
+			{
+				$join = ",$this->cat_table WHERE $this->result_table.GrpId=$this->cat_table.GrpId";
+				$extra_cols = 'name,rkey';
+			}
+			return $this->search($keys,'DISTINCT GrpId',$order ? $order : 'rkey,name',$extra_cols,'',false,'AND',false,$filter,$join);
+		}
+		// result of single person
+		return parent::read($keys,$extra_cols,$join !== true ? $join : '');
+	}
 
-		if ($intern)
-			$this->data = $data;
-
+	/**
+	 * changes the data from the db-format to our work-format
+	 *
+	 * @param array $data if given works on that array and returns result, else works on internal data-array
+	 * @return array
+	 */
+	function db2data($data=null)
+	{
+		if (!is_array($data))
+		{
+			$data =& $this->data;
+		}
+		if (count($data) && $this->source_charset)
+		{
+			$data = $GLOBALS['phpgw']->translation->convert($data,$this->source_charset);
+		}
 		return $data;
 	}
 
-	/*!
-	@function data2db
-	@abstract changes the data from our work-format to the db-format
-	@param $data if given works on that array and returns result, else works on internal data-array
-	*/
-	function data2db($data=0)
+	/**
+	 * changes the data from our work-format to the db-format
+	 *
+	 * @param array $data if given works on that array and returns result, else works on internal data-array
+	 * @return array
+	 */
+	function data2db($data=null)
 	{
-		if ($intern = !is_array($data))
-			$data = $this->data;
-
-
-		if ($intern)
-			$this->data = $data;
-
+		if (!is_array($data))
+		{
+			$data =& $this->data;
+		}
+		if (count($data) && $this->source_charset)
+		{
+			$data = $GLOBALS['phpgw']->translation->convert($data,$this->charset,$this->source_charset);
+		}
 		return $data;
 	}
-
-	/*!
-	@function read($WetId,$GrpId=0,$PerId=0)
-	@abstract read result-data from DB
-	@param $WetId,$GrpId,$PerId following combinations are supported
-	@param $WetId,   0  ,   0  : result-data is list of cats which have an result for $WetId
-	@param $WetId,$GrpId,   0  : result-data is result for the cat $GrpId
-	@param $WetId,$GrpId,$PerId: result of one single Person
-	@param    0  ,   0  ,$PerId: all the results of one person
-	*/
-	function read($WetId,$GrpId=0,$PerId=0)
+	/**
+	 * searches db for rows matching searchcriteria
+	 *
+	 * '*' and '?' are replaced with sql-wildcards '%' and '_'
+	 *
+	 * @param array/string $criteria array of key and data cols, OR a SQL query (content for WHERE), fully quoted (!)
+	 * @param boolean $only_keys True returns only keys, False returns all cols
+	 * @param string $order_by fieldnames + {ASC|DESC} separated by colons ','
+	 * @param string/array $extra_cols string or array of strings to be added to the SELECT, eg. "count(*) as num"
+	 * @param string $wildcard appended befor and after each criteria
+	 * @param boolean $empty False=empty criteria are ignored in query, True=empty have to be empty in row
+	 * @param string $op defaults to 'AND', can be set to 'OR' too, then criteria's are OR'ed together
+	 * @param int/boolean $start if != false, return only maxmatch rows begining with start
+	 * @param array $filter if set (!=null) col-data pairs, to be and-ed (!) into the query without wildcards
+	 * @param string $join='' sql to do a join, added as is after the table-name, eg. ", table2 WHERE x=y" or 
+	 *	"LEFT JOIN table2 ON (x=y)", Note: there's no quoting done on $join!
+	 * @return array of matching rows (the row is an array of the cols) or False
+	 */
+/* as it does nothing atm.
+	function search($criteria,$only_keys=True,$order_by='',$extra_cols='',$wildcard='',$empty=False,$op='AND',$start=false,$filter=null,$join='')
 	{
-		$this->WetId = $WetId;
-		$this->GrpId = $GrpId;
-		$this->PerId = $PerId;
-
-		if ($WetId > 0 && $GrpId > 0 && !$PerId)	// read complete result of comp. for cat
-		{
-			$sql = "SELECT r.*,p.* FROM $this->table_name r,$this->per_table_name p
-						WHERE r.GrpId=$GrpId AND r.PerId=p.PerId
-						ORDER BY r.platz,p.nachname,p.vorname";
-		}
-		elseif (!$WetId && !$GrpId && $PerId > 0)	// read list comps of one athlet
-		{
-			$sql = "SELECT r.*,w.*,g.rkey as gkey,g.name as gname
-						FROM $this->table_name r,$this->comp_table_name w,$this->cat_table_name g
-						WHERE r.PerId=$PerId AND r.WetId=w.WetId AND r.GrpId=g.GrpId
-						ORDER BY w.datum,gkey";
-		}
-		elseif ($WetId > 0 && !$GrpId)	// read cat-list of competition
-		{
-
-		}
-		else	// result of single person
-		{
-			return so_sql::read(array('WetId' => $WetId,'GrpId' => $GrpId,'PerId' => $PerId));
-		}
-	}
-
-	/*!
-	@function search
-	@abstract reimplmented from so_sql to be able to call data2db before save and db2data after
-	*/
-	function search($criteria,$only_keys=True,$order_by='',$extra_cols='',$wildcard='',$empty=False)
-	{
-		unset($criteria['pkte']);	// is allwas set
-		if (!$criteria['feld_pkte'])
-			unset($criteria['feld_pkte']);
-		unset($criteria['open']);
-		if (!$criteria['serie'])
-			unset($criteria['serie']);
-		$criteria['rkey'] = strtoupper($criteria['rkey']);
-
 		//$this->debug = 1;
-		return $this->so_sql_search($criteria,$only_keys,$order_by,$extra_cols,$wildcard,$empty);
-	}
-
-	/*!
-	@function names
-	@param $keys likes to limit name-list, like for so_sql.search
-	@returns array with all Competitions of form WetId => name
-	*/
-	function names($keys=array())
-	{
-		$all = $this->search($keys,False,'datum');
-
-		if (!$all)
-			return array();
-
-		while (list($key,$data) = each($all))
-			$arr[$data['WetId']] = $data['rkey'].': '.$data['name'];
-
-		return $arr;
-	}
-};
+		return parent::search($criteria,$only_keys,$order_by,$extra_cols,$wildcard,$empty,$op,$start,$filter,$join);
+	}*/
+}
