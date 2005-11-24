3d3d4e4f1d1d5f8c30f86164a16d40c8ee097c3a
ralf <ralf@87f23f9b-da0a-4d64-8e9d-2088ad7f682b>
2005-11-24 19:53:21 +0000

    adding 1.2 dependcy for the api

--- trunk/ranking/setup/setup.inc.php
+++ trunk/ranking/setup/setup.inc.php
@@ -27,12 +27,12 @@
 	/* Dependacies for this app to work */
 	$setup_info['ranking']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('1.0.0','1.0.1')
+		 'versions' => Array('1.0.0','1.0.1','1.2')
 	);
-    $setup_info['ranking']['depends'][] = array(   // this is only necessary as long the etemplate-class is not in the api
-             'appname' => 'etemplate',
-             'versions' => Array('1.0.0','1.0.1')
-    );
+	$setup_info['ranking']['depends'][] = array(
+		'appname' => 'etemplate',
+		'versions' => Array('1.0.0','1.0.1','1.2')
+	);
 
 
 
