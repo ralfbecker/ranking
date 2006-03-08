89988888a136f7aafaea77eaa13e8bd554adf313
Ralf Becker <rb@stylite.de>
2006-03-08 21:41:59 +0000

    adding 1.3 api to dependencies

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -27,11 +27,11 @@
 	/* Dependacies for this app to work */
 	$setup_info['ranking']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('1.0.0','1.0.1','1.2')
+		 'versions' => Array('1.0.0','1.0.1','1.2','1.3')
 	);
 	$setup_info['ranking']['depends'][] = array(
 		'appname' => 'etemplate',
-		'versions' => Array('1.0.0','1.0.1','1.2')
+		'versions' => Array('1.0.0','1.0.1','1.2','1.3')
 	);
 
 
