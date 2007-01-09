b0fb3ea2e6ca6240a85a99be88ca676d78aed0d5
Ralf Becker <rb@stylite.de>
2007-01-09 08:47:07 +0000

    new startlist modus: unranked starters at the end

--- inc/class.boranking.inc.php
+++ inc/class.boranking.inc.php
@@ -771,6 +771,12 @@ class boranking extends soranking
 					$reset_data = false;
 				}
 			}
+// new modus: unranked starters at the end of the startlist
+			// reverse the startlists now, to have the first in the ranking at the end of the list
+			for ($route = 1; $route <= $num_routes; ++$route)
+			{
+				$startlist[$route] = array_reverse($startlist[$route]);	
+			}
 		}
 		// now we randomly pick starters and devide them on the routes
 		while(count($starters))
@@ -782,12 +788,11 @@ class boranking extends soranking
 		for ($route = 1; $route <= $num_routes; ++$route)
 		{
 			// if we used a ranking, we have to reverse the startlist
-			if ($mode) 
-			{
-				$startlist[$route] = array_reverse($startlist[$route]);
-			}
-			//_debug_array($startlist[$route]);
-
+// old modus: unranked startes at the beginning
+//			if ($mode) 
+//			{
+//				$startlist[$route] = array_reverse($startlist[$route]);
+//			}
 			foreach($startlist[$route] as $n => $athlete)
 			{
 				// we preserve the registration number in the last 6 bit's
