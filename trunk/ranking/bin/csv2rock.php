b7d066b9d47359b7bb0cada88270377dff229158
ralf <ralf@87f23f9b-da0a-4d64-8e9d-2088ad7f682b>
2007-03-28 18:42:40 +0000

    simple filter to import startlists

---
+++ trunk/ranking/bin/csv2rock.php
@@ -0,0 +1,72 @@
+#!/usr/bin/php -qC
+<?php
+
+if (isset($_SERVER['DOCUMENT_ROOT'])) die('Commanline only!!!');
+
+$rock_db_path = '/usr/rock_db';
+
+if ($_SERVER['argc'] <= 2)
+{
+	$rock_file = 'php://stdout';
+}
+else
+{
+	list($route,$comp) = explode('.',$_SERVER['argv'][2]);
+	$rock_file = $rock_db_path.'/'.$comp.'/~'.$route.'.rte';
+}
+
+$csv_file = $_SERVER['argc'] <= 1 ? 'php://stdin' : $_SERVER['argv'][1];
+
+if (!($f = @fopen($csv_file,'r')))
+{
+	echo "File '$csv_file' not found!!!\n";
+	die("Usage: csv2rock [csv-file [route.wand]]\n");
+}
+
+if (!($csv_fields = fgetcsv($f,null,';')) || count($csv_fields) <= 1)
+{
+	die("File '$csv_file' is NO csv file!!!\n");
+}
+print_r($csv_fields);
+
+if (!($fr = @fopen($rock_file,'w')))
+{
+	die("Can't open '$rock_file' for writing!!!\n");
+}
+
+if (in_array('boulder4',$csv_fields))
+{
+	// boulder competition
+	$num_problems = in_array('boulder6',$csv_fields) ? 6 : (in_array('boulder5',$csv_fields) ? 5 : 4);
+}
+
+$max_place = 0;
+while(($line = fgetcsv($f,null,';')))
+{
+	$lines[] = $line = array_combine($csv_fields,$line);
+	if ($line['place'] > $max_place) $max_place = $line['place'];
+}
+fclose($f);
+
+// sort by abs_platz
+usort($lines,create_function('$a,$b','$aplace=$a["place"]?$a["place"]:9000+$a["startorder"]; $bplace=$b["place"]?$b["place"]:9000+$b["startorder"]; return ($aplace-$bplace) ? $aplace-$bplace : strcasecmp($a["lastname"].$a["firstname"],$b["lastname"].$b["firstname"]);'));
+foreach($lines as $key => $line)
+{
+	$lines[$key]['abs_platz'] = 1 + $key;
+}
+
+usort($lines,create_function('$a,$b','return $a["startorder"]-$b["startorder"];'));
+print_r($lines);
+
+foreach($lines as $line)
+{
+	if (!$line['place']) $line['place'] = $max_place + 1;
+
+	fwrite($fr,($line['startnumber']?$line['startnumber']:$line['startorder']).
+		"\t0\t0\t0\t0\t".	// ToDo write the real result
+		($line['place'] ? $line['place'] : $max_place+1)."\t".$line['abs_platz']."\n".
+		$line["lastname"]."\n".$line['firstname']."\n\n".$line['nation']."\n\n".$line['athlete']."\n\n");
+	
+	// ToDo write sub-routes for each bouler-problem with boulder-result
+}
+fclose($fr);
