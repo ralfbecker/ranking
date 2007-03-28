d948739c0043ff1a75e76146eea0ac3df9ecc132
Ralf Becker <rb@stylite.de>
2007-03-28 18:44:15 +0000

    simple filter to import startlists

--- bin/csv2rock.php
+++ bin/csv2rock.php
@@ -1,5 +1,17 @@
 #!/usr/bin/php -qC
 <?php
+/**************************************************************************\
+* eGroupWare - digital ROCK Rankings - filter to create rock-files         *
+* http://www.egroupware.org, http://www.digitalROCK.de                     *
+* Written and (c) by Ralf Becker <RalfBecker@outdoor-training.de>          *
+* --------------------------------------------                             *
+*  This program is free software; you can redistribute it and/or modify it *
+*  under the terms of the GNU General Public License as published by the   *
+*  Free Software Foundation; either version 2 of the License, or (at your  *
+*  option) any later version.                                              *
+\**************************************************************************/
+
+/* $Id$ */
 
 if (isset($_SERVER['DOCUMENT_ROOT'])) die('Commanline only!!!');
 
