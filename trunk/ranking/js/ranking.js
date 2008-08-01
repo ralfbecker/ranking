00b1ad2006954ee82bcdcf304bbe5a4210060ed5
ralfbecker <ralfbecker@87f23f9b-da0a-4d64-8e9d-2088ad7f682b>
2008-08-01 10:15:07 +0000

    javascript helpers to enforce common top/bonus rules

--- trunk/ranking/js/ranking.js
+++ trunk/ranking/js/ranking.js
@@ -6,8 +6,8 @@
  * @link http://www.egroupware.org
  * @link http://www.digitalROCK.de
  * @author Ralf Becker <RalfBecker@digitalrock.de>
- * @copyright 2007 by Ralf Becker <RalfBecker@digitalrock.de>
- * @version $Id$ 
+ * @copyright 2007/8 by Ralf Becker <RalfBecker@digitalrock.de>
+ * @version $Id$
  */
 
 /**
@@ -18,19 +18,58 @@
  */
 function handle_plus(height,plus_sel_id)
 {
-	if (height.value.match(/(t|top|\+|\-)$/i)) 
-	{ 
-		var plus = height.value.replace(/(.*)(t|top|\+|\-)$/i,'$2'); 
-	
-		document.getElementById(plus_sel_id).value = plus == '+' ? 1 : (plus == '-' ? -1 : 9999); 
+	if (height.value.match(/(t|top|\+|\-)$/i))
+	{
+		var plus = height.value.replace(/(.*)(t|top|\+|\-)$/i,'$2');
+
+		document.getElementById(plus_sel_id).value = plus == '+' ? 1 : (plus == '-' ? -1 : 9999);
 		height.value = height.value.replace(/(.*)(t|top|\+|\-)$/i,'$1');
+	}
+}
+
+/**
+ * onChange of top: if bonus not set, set it to the same number of tries as top
+ *  if top < bonus alert user and set top to bonus
+ *
+ * @param top top select box
+ */
+function check_top(top)
+{
+	var bonus = document.getElementById(top.name.replace(/top/g,'zone'));
+
+	if (bonus && !bonus.value && top.value > 0) bonus.value = top.value;
+
+	if (bonus && top.value > 0 && top.value < bonus.value)
+	{
+		alert('Top < Bonus!');
+		bonus.value = top.value;
+	}
+}
+
+/**
+ * onChange of bonus: dont allow to set a bonus bigger then top or no bonus, but a top
+ *
+ * @param top top select box
+ */
+function check_bonus(bonus)
+{
+	var top = document.getElementById(bonus.name.replace(/zone/g,'top'));
+
+	if (top && top.value > 0 && bonus.value > top.value)
+	{
+		alert('Bonus > Top!');
+		top.value = bonus.value;
+	}
+	if (top && top.value > 0 && !bonus.value)
+	{
+		top.value = bonus.value;
 	}
 }
 
 function start_time_measurement(button,athlete)
 {
-	set_style_by_class('td','ajax-loader','display','inline'); 
-	document.getElementById('msg').innerHTML='Time measurement'; 
-	
+	set_style_by_class('td','ajax-loader','display','inline');
+	document.getElementById('msg').innerHTML='Time measurement';
+
 	xajax_doXMLHTTP('ranking.uiresult.ajax_time_measurement',button.form.etemplate_exec_id.value,athlete);
 }
