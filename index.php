9c552cc1d2d32247e8dbc40b53b128574bd29672
Ralf Becker <rb@stylite.de>
2012-01-14 00:51:19 +0000

    fix old class-names stored in user prefs

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * eGroupWare digital ROCK Rankings
+ * EGroupware digital ROCK Rankings
  *
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @package ranking
  * @link http://www.egroupware.org
  * @link http://www.digitalROCK.de
  * @author Ralf Becker <RalfBecker@digitalrock.de>
- * @copyright 2006/7 by Ralf Becker <RalfBecker@digitalrock.de>
- * @version $Id$ 
+ * @copyright 2006-12 by Ralf Becker <RalfBecker@digitalrock.de>
+ * @version $Id$
  */
 
 $GLOBALS['egw_info'] = array(
@@ -19,10 +19,19 @@ $GLOBALS['egw_info'] = array(
 ));
 include('../header.inc.php');
 
-if (!($view = $GLOBALS['egw']->session->appsession('menuaction','ranking')))
+if (!($view = $GLOBALS['egw']->session->appsession('menuaction','ranking')) &&
+	!($view = $GLOBALS['egw_info']['user']['preferences']['ranking']['default_view']))
 {
-	$view = $GLOBALS['egw_info']['user']['preferences']['ranking']['default_view'];
+	$view = 'ranking.uiranking.index';
 }
-ExecMethod($view ? $view : 'ranking.uiranking.index');
+// fix old class-names stored in user prefs
+$old2new = array(
+	'uicompetitions' => 'ranking_competition_ui',
+	'uicups' => 'ranking_cup_ui',
+	'uiathlets' => 'ranking_athlete_ui',
+);
+if (isset($old2new[$view])) $view = $old2new[$view];
 
-$GLOBALS['egw']->common->egw_footer();
+ExecMethod($view);
+
+common::egw_footer();
