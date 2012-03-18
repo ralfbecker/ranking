a70e25b3131a597657b9378fefd6e476005b8ee5
ralfbecker <ralfbecker@87f23f9b-da0a-4d64-8e9d-2088ad7f682b>
2012-03-18 07:45:45 +0000

    fixed cat_id's of international competitions

--- trunk/ranking/sitemgr/digitalrock/dav_calendar.php
+++ trunk/ranking/sitemgr/digitalrock/dav_calendar.php
@@ -92,7 +92,7 @@ $cats = array(
 		'rang_title'=> 'CUWR continuously updated WORLDRANKING',
 		'bgcolor'   => '#B8C8FF',
 		'nat_team_ranking' => !isset($_REQUEST['year']) || $_REQUEST['year'] >= 2005,
-		'cat_id' => array(52,53,54,68,69,70),
+		'cat_id' => array(68,69,70,86),
 	),
 	'youth' => array(
 		'label'  => 'Int. Jugend',
@@ -102,7 +102,7 @@ $cats = array(
 		'serie_reg' => '^[0-9]{2,2}_EYC',
 		'rang_title'=> '',
 		'bgcolor'   => '#D8E8FF',
-		'cat_id' => array(55),
+		'cat_id' => array(71),
 	),
 	'ger_boulder' => array(
 		'label'  => 'Bouldern',
