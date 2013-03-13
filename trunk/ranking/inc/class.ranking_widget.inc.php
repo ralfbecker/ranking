d24a347c71ec80beae9f6099c6423964b3ea52d1
ralfbecker <ralfbecker@87f23f9b-da0a-4d64-8e9d-2088ad7f682b>
2013-03-13 22:34:40 +0000

    first version for Resultlist widget, should be easy to extend for rankings and startlist

--- trunk/ranking/inc/class.ranking_widget.inc.php
+++ trunk/ranking/inc/class.ranking_widget.inc.php
@@ -106,6 +106,13 @@ class ranking_widget
 	}
 
 	/**
+	 * Query parameters
+	 *
+	 * @var array
+	 */
+	protected $params = array();
+
+	/**
 	 * Render HTML or javascript for widget
 	 *
 	 * HTML is only rendered if $_GET['_escaped_fragment_'] is set
@@ -118,10 +125,10 @@ class ranking_widget
 	{
 		if (isset($_GET[self::FRAGMENT]))
 		{
-			parse_str($_GET[self::FRAGMENT], $params);
+			parse_str($_GET[self::FRAGMENT], $this->params);
 
 			try {
-				return $this->render_widgets($params, $id, $title);
+				return $this->render_widgets($id, $title);
 			}
 			catch (Exception $e) {
 				if ($this->throw_error)
@@ -173,27 +180,42 @@ class ranking_widget
 	protected $data;
 
 	/**
+	 * Parameters support by this widget
+	 *
+	 * @var array
+	 */
+	public static $supported_params = array('person','comp','cat','route','nation','year','filter','type');
+
+	/**
+	 * URL without our parameters
+	 *
+	 * @var string
+	 */
+	protected $page_url;
+
+	/**
 	 * Render widget server-side depending on given parameters
 	 *
-	 * @param array $params
 	 * @param string $id='widget_content'
 	 * @param string &$title on return of server-side rendering title of widget, to eg. put in page-title
 	 * @return html
 	 */
-	public function render_widgets(array $params, $id='widget_content', &$title=null)
+	public function render_widgets($id='widget_content', &$title=null)
 	{
 		$query = '';
-		foreach(array('person','comp','cat','nation','year','filter','type') as $name)
+		foreach(self::$supported_params as $name)
 		{
-			if (isset($params[$name]))
+			if (isset($this->params[$name]))
 			{
-				foreach((array)$params[$name] as $key => $value)
+				foreach((array)$this->params[$name] as $key => $value)
 				{
 					$query .= ($query ? '&' : '?').
 						$name.(is_string($key) ? '['.$key.']' : '').'='.urlencode($value);
 				}
 			}
 		}
+		$this->page_url = preg_replace('/('.implode('|', self::$supported_params).'|'.self::FRAGMENT.')=[^&]*(&|$)/', '', $_SERVER['REQUEST_URI']);
+
 		//echo "<pre>".print_r($params, true)."</pre>\n"; echo $query;
 		if (!($json = file_get_contents($this->json_url.$query)))
 		{
@@ -213,14 +235,357 @@ class ranking_widget
 		{
 			return $this->Starters($id, $title);
 		}
+		if (isset($this->data['participants']))// || isset($this->data['categorys']))
+		{
+			return $this->Resultlist($id, $title);
+		}
 		return "Not yet implemented!";
+	}
+
+	private $result_cols = array();
+
+	/**
+	 * Server-side rendering of results, rankings and startlists
+	 *
+	 * @param string $id='widget_content'
+	 * @param string &$title on return of server-side rendering title of widget, to eg. put in page-title
+	 * @param string $result=true false: startlist
+	 * @return html
+	 */
+	protected function Resultlist($id, &$title=null, $result=true)
+	{
+		$title = $this->data['comp_name'];
+		$content = $this->tag('h1', $title, array('class' => 'compHeader'));
+		if (!empty($this->data['route_result']))
+		{
+			$content .= $this->tag('h3', $this->data['route_result'], array('class' => 'resultDate'));
+		}
+		$content .= $this->tag('h1', $this->data['route_name'], array('class' => 'listHeader'));
+
+		if ((!isset($this->params['toc']) || $this->params['toc']) &&
+			(!isset($this->params['beamer']) || !$this->params['beamer']) &&
+			$this->data['discipline'] != 'ranking')
+		{
+			$toc = $gen = '';
+			$page_url = preg_replace('/(comp|cat|route|'.self::FRAGMENT.')=[^&]*(&|$)/', '', $_SERVER['REQUEST_URI']).
+				'#!comp='.$this->data['WetId'].'&cat='.$this->data['GrpId'].'&route=';
+			foreach($this->data['route_names'] as $r => $name)
+			{
+				if ($r != $this->data['route_order'])
+				{
+					$li = $this->tag('li', $this->tag('a', $name, array(
+						'href' => $page_url.$r,
+					)), array(), false);
+				}
+				else
+				{
+					$li = $this->tag('li', $name);
+				}
+				if ($r == -1)
+				{
+					$gen = $li;
+				}
+				else
+				{
+					$toc = $li.$toc;
+				}
+			}
+			$content .= $this->tag('ul', $gen.$toc, array('class' => 'listToc'), false);
+		}
+
+		$this->result_cols = $this->result_cols($result);
+		//echo "<pre>".print_r($this->result_cols, true)."</pre>\n";
+
+		// header
+		$thead = $this->createRow($this->result_cols, 'th');
+
+		// ahtletes
+		$tbody = '';
+		foreach($this->data['participants'] as $data)
+		{
+			if (isset($data['results']) && is_array($data['results']))
+			{
+				if (!empty($data['name']))
+				{
+					$th = htmlspecialchars($data['name'], ENT_COMPAT, $this->charset)."\n";
+					if (!empty($data['url']))
+					{
+						$th .= $this->tag('a', $this->lang('complete result'), array(
+							'href' => $data['url'],
+						));
+						$quote = false;
+					}
+					$tbody .= $this->tag('tr', $this->tag('th', $th, array(
+						'colspan' => count($this->result_cols),
+					), false), array(), false);
+				}
+				foreach($data['results'] as $data)
+				{
+					$tbody .= $this->createRow($data);
+				}
+			}
+			else	// single result row
+			{
+				if ($result && (empty($data['result_rank']) || $data['result_rank'] < 1))
+				{
+					break;	// no more ranked competitiors
+				}
+				$tbody .= $this->createRow($data);
+			}
+		}
+		$content .= $this->tag('table',
+			$this->tag('thead', $thead, array(), false).
+			$this->tag('tbody', $tbody, array(), false),
+			array(
+				'class' => 'DrTable',
+			), false);
+
+		return $this->tag('div', $content, array(
+			'id' => $id,
+			'class' => 'Resultlist',
+		), false);
+	}
+
+	/**
+	 * Create new data-row with all columns from this.columns
+	 */
+	private function createRow($data, $tag='td')
+	{
+		$row = '';
+		$span = 1;
+		foreach($this->result_cols as $col => $label_data)
+		{
+			if (--$span > 0) continue;
+
+			$url = isset($data['url']) ? $data['url'] : null;
+			$col_data = isset($data[$col]) ? $data[$col] : null;
+			// allow /-delemited expressions to index into arrays and objects
+			if (!isset($col_data) || strpos($col, '/') !== false)
+			{
+				$col_data = $data;
+				foreach(explode('/', $col) as $col)
+				{
+					if ($col == 'lastname' || $col == 'firstname')
+					{
+						$url = $col_data['url'];
+					}
+					$col_data = isset($col_data[$col]) ? $col_data[$col] : null;
+				}
+			}
+			elseif (strpos($col, '/') !== false)
+			{
+				$col = substr($col, strrpos($col, '/'));
+			}
+			$quote = true;
+			switch($col)
+			{
+				case 'firstname':
+				case 'lastname':
+					if (!empty($url))
+					{
+						$col_data = $this->tag('a', $col_data, array(
+							'href' => $this->page_url.'#!person='.$data['PerId'].'&cat='.$this->data['GrpId'],
+						));
+						$quote = false;
+					}
+					break;
+				case 'nation':
+				case 'federation':
+					if (!empty($data['fed_url']))
+					{
+						$col_data = $this->tag('a', $col_data, array(
+							'href' => $data['fed_url'],
+							'target' => '_blank',
+						));
+						$quote = false;
+					}
+					break;
+			}
+			if (is_array($col_data))
+			{
+				if (isset($col_data['colspan']) && $col_data['colspan'] > 1) $span = $col_data['colspan'];
+				if (!empty($col_data['url']))
+				{
+					$col_data = $this->tag('a', $col_data['label'], array(
+						'href' => $col_data['url'],
+					));
+					$quote = false;
+				}
+				else
+				{
+					$col_data = $col_data['label'];
+				}
+			}
+			$row .= $this->tag($tag, empty($col_data) ? '' : $col_data, array(
+				'class' => $col,
+			)+($span > 1 ? array('colspan' => $span) : array()), $quote);
+		}
+		$attr = array();
+		// add or remove quota line
+		if (isset($this->result_cols['result_rank']) && !empty($this->data['quota']) && !empty($data['result_rank']) &&
+			$data['result_rank'] >= 1 && $data['result_rank'] > $this->data['quota'])
+		{
+			$attr = array('class' => 'quota_line');
+			unset($this->data['quota']);	// to set quota line only once
+		}
+		return $this->tag('tr', $row, $attr, false);
+	}
+
+	/**
+	 * Get columns for result display
+	 *
+	 * @param string $result=true false: startlist
+	 * @todo Startlist cols
+	 * @return array column => label pairs
+	 */
+	private function result_cols($result = true)
+	{
+		$detail = isset($this->params['detail']) ? $this->params['detail'] : null;
+
+		switch($this->data['discipline'])
+		{
+			case 'speedrelay':
+				$cols = empty($detail) ? array(	// default detail
+					'result_rank' => 'Rank',
+					'team_name' => 'Teamname',
+					'athletes/0/lastname' => 'Athlete #1',
+					'athletes/1/lastname' => 'Athlete #2',
+					'athletes/2/lastname' => 'Athlete #3',
+					'result' => 'Sum'
+				) : ($detail == '1' ? array(	// detail=1
+					'result_rank' => 'Rank',
+					'team_name' => 'Teamname',
+					//'team_nation' => 'Nation',
+					'athletes/0/lastname' => array('label' => 'Athlete #1', 'colspan' => 3),
+					'athletes/0/firstname' => '',
+					'athletes/0/result_time' => '',
+					'athletes/1/lastname' => array('label' => 'Athlete #2', 'colspan' => 3),
+					'athletes/1/firstname' => '',
+					'athletes/1/result_time' => '',
+					'athletes/2/lastname' => array('label' => 'Athlete #3', 'colspan' => 3),
+					'athletes/2/firstname' => '',
+					'athletes/2/result_time' => '',
+					'result' => 'Sum'
+				) : array(	// detail=0
+					'result_rank' => 'Rank',
+					'team_name' => 'Teamname',
+					'team_nation' => 'Nation',
+					'result' => 'Sum'
+				));
+				break;
+
+			case 'ranking':
+				$cols = array(
+					'result_rank' => 'Rank',
+					'lastname' => array('label' => 'Name', 'colspan' => 2),
+					'firstname' => '',
+					'nation' => 'Nation',
+					'points' => 'Points',
+					'result' => 'Result'
+				);
+				if (empty($detail))
+				{
+					unset($cols['result']);
+					// allow to click on points to show single results
+					$cols['points'] = array(
+						'label' => $cols['points'],
+						'url' => location.href+'&detail=1'
+					);
+				}
+				break;
+
+			default:
+				$cols = isset($detail) && $detail == '0' ? array(
+					'result_rank' => 'Rank',
+					'lastname' => array('label' => 'Name', 'colspan' => 2),
+					'firstname' => '',
+					'nation' => 'Nation',
+					'result' => 'Result'
+				) : array(
+					'result_rank' => 'Rank',
+					'lastname' => array('label' => 'Name', 'colspan' => 2),
+					'firstname' => '',
+					'nation' => 'Nation',
+					'start_number' => 'StartNr',
+					'result' => 'Result'
+				);
+				break;
+		}
+		// for SUI and GER competitions replace nation
+		if (!empty($this->data['nation']))
+		{
+			switch ($this->data['nation'])
+			{
+				case 'GER':
+					$this->replace_attributes($cols, 'nation', 'federation', 'DAV Sektion');
+					break;
+				case 'SUI':
+					$this->replace_attribute($cols, 'nation', 'city', 'City');
+					break;
+			}
+		}
+		// for general result use one column per heat
+		if (isset($cols['result']) && $this->data['route_names'] && $this->data['route_order'] == -1)
+		{
+			unset($cols['result']);
+			// show final first and 2. quali behind 1. quali: eg. 3, 2, 0, 1
+			foreach(array_reverse($this->data['route_names'], true) as $route => $name)
+			{
+				if ($route != 1 && isset($this->data['route_names'][abs($route)]))
+				{
+					$cols['result'.abs($route)] = $this->data['route_names'][abs($route)];
+				}
+			}
+			// evtl. add points column
+			if (!empty($this->data['participants'][0]['quali_points']))
+			{
+				$cols['quali_points'] = 'Points';
+			}
+			$title_prefix = '';
+		}
+		if (!empty($cols['result']) && !empty($this->data['participants'][0]['rank_prev_heat']) &&
+			(!isset($detail) || $detail != 0))
+		{
+			$cols['rank_prev_heat'] = 'previous heat';
+		}
+		return $cols;
+	}
+
+	/**
+	 * Replace attribute named from with one name to and value keeping the order of the attributes
+	 *
+	 * @param object obj
+	 * @param string from
+	 * @param string to
+	 * @param mixed value
+	 */
+	private function replace_attribute(array &$attrs, $from, $to, $value)
+	{
+		$found = false;
+		foreach($attrs as $attr => $val)
+		{
+			if (!$found)
+			{
+				if ($attr == $from)
+				{
+					$found = true;
+					unset($attrs[$attr]);
+					$attrs[$to] = $value;
+				}
+			}
+			else
+			{
+				unset($attrs[$attr]);
+				$attrs[$attr] = $val;
+			}
+		}
 	}
 
 	protected $fed_rows = array();
 	protected $fed_rows_pos = array();
 
 	/**
-	 * Server-side rendering of calendar
+	 * Server-side rendering of starters / registration
 	 *
 	 * @param string $id='widget_content'
 	 * @param string &$title on return of server-side rendering title of widget, to eg. put in page-title
@@ -296,7 +661,10 @@ class ranking_widget
 			$this->tag('tfoot', $tfoot, array(), false),
 			array('class' => 'DrTable'), false);
 
-		return $this->tag('div', $content, array('class' => 'Starters'), false);
+		return $this->tag('div', $content, array(
+			'id' => $id,
+			'class' => 'Starters',
+		), false);
 	}
 
 	/**
