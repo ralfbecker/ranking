05f231a7f501328dd4c6d174678735f677ad8a65
Ralf Becker <rb@stylite.de>
2015-03-20 02:14:10 +0000

    apply buttons in result-rows only submit partial content and got checking of whole boulder result rows working again

--- js/app.js
+++ js/app.js
@@ -162,9 +162,30 @@ app.classes.ranking = AppJS.extend(
 	 * @param {jQuery.Event} _event
 	 * @param {et2_button|et2_checkbox} _widget
 	 */
-   update_result_row: function(_elem, _widget)
-   {
+	update_result_row: function(_event, _widget)
+	{
+		if (!_widget) _widget = _event;	// click seems to only set button as 1. param
 		var parent = _widget.getParent();
+		// boulder "checked" checkbox
+		if (_widget.instanceOf(et2_checkbox) && parent._children.length == 2)
+		{
+			// readonly apply button --> use ajax call to uncheck
+			if (parent._children[1].options.readonly)
+			{
+				var content = _widget.getRoot().getArrayMgr('content').data;
+				this.egw.json('ranking.ranking_result_ui.ajax_uncheck_result', [{
+					WetId: content.WetId,
+					GrpId: content.GrpId,
+					route: content.route_order,
+					PerId: _widget.id.replace(/^set\[([0-9]+)\]\[checked\]$/, '$1')
+				}]).sendRequest();
+			}
+			else	// to check --> just click on apply
+			{
+				parent._children[1].click();
+			}
+			return;
+		}
 		while(parent && !parent.instanceOf(et2_grid))
 		{
 			parent = parent.getParent();
@@ -173,5 +194,128 @@ app.classes.ranking = AppJS.extend(
 		{
 			_widget.getInstanceManager().submit(null, false, false, parent.getRow(_widget));
 		}
-   }
+	},
+
+	/**
+	 * onChange of top: if bonus not set, set it to the same number of tries as top
+	 *  if top < bonus alert user and set top to bonus
+	 *
+	 * @param {DOMNode} node
+	 * @param {et2_selectbox} top select box
+	 */
+	check_top: function(node, top)
+	{
+		var top_value = top ? top.get_value() : undefined;
+		var bonus = this.et2.getWidgetById(top.id.replace(/top/g,'zone'));
+		var bonus_value = bonus ? bonus.get_value() : undefined;
+
+		if (bonus && (!bonus_value || bonus_value == '0') && parseInt(top_value) > 0) bonus.set_value(bonus_value=top_value);
+
+		if (bonus && parseInt(top_value) > 0 && parseInt(top_value) < parseInt(bonus_value))
+		{
+			//window.setTimeout(function(){	// work around iOS bug crashing Safari
+				alert('Top < Bonus!');
+				bonus.set_value(top_value);
+			//}, 10);
+		}
+	},
+
+	/**
+	 * onChange of tops: if bonus not set, set it to the same number of tops
+	 *  if tops > zones alert user and set tops to zones or
+	 *  if less tries then tops alert user and set tries to tops
+	 *
+	 * @param {DOMNode} node
+	 * @param {et2_selectbox} top select box
+	 */
+	check_tops: function(node, top)
+	{
+alert('Not yet implemented :-('); return;
+		var bonus = document.getElementById(top.name.replace(/tops/g,'zones'));
+
+		if (bonus && !bonus.value && parseInt(top.value) > 0) bonus.value = top.value;
+
+		if (bonus && parseInt(top.value) > 0 && parseInt(top.value) > parseInt(bonus.value))
+		{
+			window.setTimeout(function(){	// work around iOS bug crashing Safari
+				alert('Top > Bonus!');
+				bonus.value = top.value;
+			}, 10);
+		}
+
+		var tries = document.getElementById(top.name.replace(/tops/g,'top_tries'));
+
+		if (tries && !tries.value && parseInt(top.value) > 0) tries.value = top.value;
+
+		if (tries && parseInt(top.value) > 0 && parseInt(top.value) > parseInt(tries.value))
+		{
+			window.setTimeout(function(){	// work around iOS bug crashing Safari
+				alert('Top > Tries!');
+				tries.value = top.value;
+			}, 10);
+		}
+	},
+
+	/**
+	 * onChange of bonus: dont allow to set a bonus bigger then top or no bonus, but a top
+	 *
+	 * @param {DOMNode} node
+	 * @param {et2_selectbox} bonus select box
+	 */
+	check_bonus: function(node, bonus)
+	{
+		var bonus_value = bonus ? bonus.get_value() : undefined;
+		var top = this.et2.getWidgetById(bonus.id.replace(/zone/g,'top'));
+		var top_value = top ? top.get_value() : undefined;
+
+		if (top && parseInt(top_value) > 0 && parseInt(bonus_value) > parseInt(top_value))
+		{
+			//window.setTimeout(function(){	// work around iOS bug crashing Safari
+				alert('Bonus > Top!');
+				top.set_value(top_value=bonus_value);
+			//}, 10);
+		}
+		if (top && parseInt(top_value) > 0 && !bonus_value)
+		{
+			top.set_value(bonus_value);
+		}
+	},
+
+	/**
+	 * onChange of zones: dont allow to set a zones bigger then tops or no zones, but a top
+	 * 	or if less tries then zones alert user and set tries to zones
+	 *
+	 * @param {DOMNode} node
+	 * @param {DOMNode} bonus select box
+	 */
+	check_boni: function(node, bonus)
+	{
+alert('Not yet implemented :-('); return;
+		var top = document.getElementById(bonus.name.replace(/zones/g,'tops'));
+
+		if (top && parseInt(top.value) > 0 && parseInt(bonus.value) < parseInt(top.value))
+		{
+			window.setTimeout(function(){	// work around iOS bug crashing Safari
+				alert('Bonus < Top!');
+				top.value = bonus.value;
+			}, 10);
+		}
+		if (top && parseInt(top.value) > 0 && !bonus.value)
+		{
+			top.value = bonus.value;
+		}
+
+		var tries = document.getElementById(bonus.name.replace(/zones/g,'zone_tries'));
+
+		if (tries && !tries.value && parseInt(bonus.value) > 0) tries.value = bonus.value;
+
+		if (tries && parseInt(bonus.value) > 0 && parseInt(bonus.value) > parseInt(tries.value))
+		{
+			window.setTimeout(function(){	// work around iOS bug crashing Safari
+				alert('Bonus > Tries!');
+				tries.value = bonus.value;
+			}, 10);
+		}
+	}
+
 });
