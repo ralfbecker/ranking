bc1971edd809355a4221597196cef98b17d3c7fc
Ralf Becker <RalfBecker@outdoor-training.de>
2018-05-12 18:55:11 +0300

    fix error deleting athlete from startlist

--- inc/class.ranking_route_result.inc.php
+++ inc/class.ranking_route_result.inc.php
@@ -1839,7 +1839,7 @@ class ranking_route_result extends Api\Storage\Base
 				'WetId' => $data['WetId'],
 				'GrpId' => $data['GrpId'],
 				'route_order' => $data['route_order'],
-			))->fetchColumn();
+			), __LINE__, __FILE__)->fetchColumn();
 
 			// Example for 21 starters: 1. in Lane A will be 11. in LaneB
 			$this->db->query("UPDATE $this->table_name SET start_order2n=1+((FLOOR($num_participants/2)+start_order-1) % $num_participants) WHERE ".
