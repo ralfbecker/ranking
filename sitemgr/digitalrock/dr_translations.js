ca1094f730934b087ff696af707fd36298f7d490
Ralf Becker <RalfBecker@outdoor-training.de>
2018-07-02 08:46:27 +0200

    missing German translations ;)

---
+++ sitemgr/digitalrock/dr_translations.js
@@ -0,0 +1,82 @@
+var dr_translations = {
+	'after': {
+		de: 'nach' },
+	'Agegroup': {
+		de: 'Jahrgang' },
+	'Athlete #1': {
+		de: 'Athlete #1' },
+	'Athlete #2': {
+		de: 'Athlete #2' },
+	'Athlete #3': {
+		de: 'Athlete #3' },
+	'Athlete asked not to show his name anymore.': {
+		de: 'Der Athlet hat gebeten seinen Namen nicht mehr anzuzeigen.' },
+	'As of': {
+		de: 'Stand' },
+	'Birthyear': {
+		de: 'Jahrgang' },
+	'Calendar': {
+		de: 'Kalender' },
+	'City': {
+		de: 'Stadt' },
+	'Deadline': {
+		de: 'Anmeldeschluss' },
+	'Event Website': {
+		de: 'Event Webseite' },
+	'Final Points': {
+		de: 'Final Punkte' },
+	'For %1 %2 best results per competition and category are counting.': {
+		de: 'Für %1 zählen die %2 besten Ergebnisse pro Wettkampf und Kategorie.' },
+	'Info Sheet': {
+		de: 'Infoblatt' },
+	'Name': {
+		de: 'Name' },
+	'Nation': {
+		de: 'Nation' },
+	'Not counting results are in brackets.': {
+		de: 'Nicht zählende Ergebnise sind in Klammern.' },
+	'Points': {
+		de: 'Punkte' },
+	'Qualification': {
+		de: 'Qualifikation' },
+	'Rank': {
+		de: 'Rang' },
+	'Regionalzentrum': {
+		de: 'Regionalzentrum' },
+	'Regulation': {
+		de: 'Regulation' },
+	'Result': {
+		de: 'Ergebnis' },
+	'See also:': {
+		de: 'Siehe auch:' },
+	'Sektion': {
+		de: 'Sektion' },
+	'Select a single category to show ...': {
+		de: 'Einzelne Kategorie zum Anzeigen auswählen ...' },
+	'Select another competition ...': {
+		de: 'Anderen Wettkampf auswählen ...' },
+	'StartNr': {
+		de: 'StartNr' },
+	'Starters': {
+		de: 'Meldeliste' },
+	'Startlist': {
+		de: 'Startliste' },
+	'Sum': {
+		de: 'Summe' },
+	'Teamname': {
+		de: 'Teamname' },
+	'Total of %1 athletes registered in all categories.': {
+		de: 'Total of %1 athletes registered in all categories.' },
+	'Vorqualifiziert': {
+		de: 'Vorqualifiziert' },
+	'calculation of this ranking': {
+		de: 'Berechnung dieser Rangliste' },
+	'complete result': {
+		de: 'komplettes Ergebnis' },
+	'previous heat': {
+		de: 'vorherige Runde' },
+	'provisional Result': {
+		de: 'provisorisches Ergebnis' },
+	'years, since': {
+		de: 'Jahre, seit' }
+};
