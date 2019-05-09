bc021a6aa097e7decef0702a9d8df84a863270c6
Ralf Becker <RalfBecker@outdoor-training.de>
2019-05-09 09:59:41 +0200

    add CUWR-rules document to doc folder

---
+++ doc/CUWR_rules.php
@@ -0,0 +1,684 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<HTML>
+<HEAD>
+   <TITLE>IFSC Worldranking</TITLE>
+   <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-1">
+   <META NAME="GENERATOR" CONTENT="Mozilla/3.0Gold (Win95; I) [Netscape]">
+   <META NAME="Author" CONTENT="Ralf Becker, digital ROCK">
+</HEAD>
+<BODY>
+
+<TABLE CELLPADDING=35 >
+<TR>
+<TD>
+<TABLE>
+<TR>
+<TD><a href="http://www.ifsc-climbing.org"><img src="ifsc-100.gif" alt="IFSC" border="0"></a></TD>
+
+<TD>
+<H1><B>IFSC Worldranking (WR)</B> </H1>
+</TD>
+</TR>
+</TABLE>
+
+<H2><U>Introduction</U> </H2>
+
+<P>These ranking system is similar to the IFSC-World Cup,
+but the <B><U>strength of the field</U></B> is taken into account by a
+factor between 1 (best possible field) and 0 - called <B><U>field-factor</U></B>.
+</P>
+
+<P>The WR is calculated (since 1999) as the <u><b>sum of all results of the last 12 month</u></b>.</p>
+
+<P>There was a minor change of the WR system starting in the
+year 1999.<br>
+<i>All changes are documented here in italics:<br>
+Before 1999 the WR was sum of the <U>best 7 results</U> out of the <U>last
+10 events</U>.
+</i></P>
+
+<H2>1. <U>field-factor</U> </H2>
+
+<P>The reason for a field factor is clear: the weight of
+a competition is chosen by the competitors who come to the competition
+and not by a commission on a green table a year before. </P>
+
+<P>The WR itself is used to get the factor (on the date
+one day before the competition). Adding the points (using the a new table,
+later called <A HREF="#cuwr-table">WR-table</A>, which is the regular
+IFSC-table plus 15 points for every place, e.g. 115 for the first) of every
+competitor who got a place in the competition leads to a sum. </P>
+
+<P>In case of ties we add the points of the different places
+and divide them by the number of ties, e.g. two 2. places: (80+15 + 65+15)
+/ 2 = 87.5 for both. </P>
+
+<P>Dividing this sum by the sum of the points of all places
+we got a factor between 0 and 1. </P>
+
+<P>So a masters-competition with only 10 invited climbers
+will have a field factor less than a regular worldcup. </P>
+
+<P>So all competitors who are under the first 30 of the actual
+WR count for the field-factor not only the ones who gets points for this
+competition (the first 30). </P>
+
+<P>The field-factor is rounded to two digits behind the point
+before the calculation of the individual points for the climbers.<BR>
+</P>
+
+<H2><B>2. <U>mode of the WR</U></B>
+</H2>
+
+<P>The WR is calculated since 1999 as the sum of <u><b>all</b></u> (counting) competitions of the <b><u>last 12 month</u></b> (exactly 1 year back).</p>
+
+<i>
+<P><b>Before 1999</b><br>
+The <U>last 10 competitions</U> are used, because
+it is nearly one year but it doesn't have the problems of a ranking which
+substitutes one competition by the 'same one' this year nor the ranking
+differs on the days between the competitions as it will do if it uses the
+competitions of exact 1 year (e.g. the ranking of 15.01.93 will consist
+of all competitions between 16.01.92 and 15.01.93).<br>
+The <U>best 7 results</U> of a climber are added
+for the WR.<br>
+To get a larger list and to give one the opportunity to
+show up in the ranking with one good placement their will be <U>no minimum
+number of competitions</U> to be ranked.</P>
+</i>
+
+<H2><B>3. <U>places and points</U> </B></H2>
+
+<P>To assign points to the competitors we use the IFSC-table
+multiplying it with the field factor. </P>
+
+<P>All points are rounded to two digits behind the point.<BR></P>
+
+<i>
+<P><b>Before 1999</b><br>
+Because a 12. place in a masters with only 15 competitors
+is not comparable with a 12. place in a world-cup with 60 competitors not
+all climbers (if we had less then 30) can get points.<br>
+A competitor has to leave at least the half field behind
+him to get points. (In the above mentioned case of a competition with only
+15 participants points will be assigned only to the first 7).</P>
+</i>
+
+<H2><B>4. <U>the start of the WR</U></B>
+</H2>
+
+<P>Because the WR itself is used for the calculation of
+the field-factor their have to be a kind of provisional ranking before
+the WR can be started: </P>
+
+<P>The provisional ranking (for the OnSight cathegory) was
+established using a fixed factor of 0.60 and the first 5 IFSC Competitions
+in 1991 (4 World Cups and the World Championship).</p><p>
+The speed and boulder WR's used a similar methode.<BR>
+</P>
+
+<H2><B>5. <U>example</U>: field-factor-calculation for Arco
+'93</B> </H2>
+
+<TABLE BORDER=1 CELLPADDING=5 >
+<TR>
+<TD>
+rank 
+</TD>
+
+<TD>
+name
+</TD>
+
+<TD></TD>
+
+<TD>
+nation 
+</TD>
+
+<TD>
+rank/ex.
+</TD>
+
+<TD>
+points 
+</TD>
+
+<TD>
+points
+</TD>
+</TR>
+
+<TR>
+<TD>
+1. 
+</TD>
+
+<TD>
+CHEVIEUX
+</TD>
+
+<TD>
+Elie 
+</TD>
+
+<TD>
+SUI 
+</TD>
+
+<TD>
+7. / 1
+</TD>
+
+<TD>
+58.00 
+</TD>
+
+<TD>
+66.00
+</TD>
+</TR>
+
+<TR>
+<TD>
+2. 
+</TD>
+
+<TD>
+LOMBARD
+</TD>
+
+<TD>
+Fran&ccedil;ois 
+</TD>
+
+<TD>
+FRA 
+</TD>
+
+<TD>
+12. / 1
+</TD>
+
+<TD>
+43.00 
+</TD>
+
+<TD>
+52.80
+</TD>
+</TR>
+
+<TR>
+<TD>
+3. 
+</TD>
+
+<TD>
+LEGRAND
+</TD>
+
+<TD>
+Fran&ccedil;ois 
+</TD>
+
+<TD>
+FRA 
+</TD>
+
+<TD>
+1. / 1
+</TD>
+
+<TD>
+115.00 
+</TD>
+
+<TD>
+42.90
+</TD>
+</TR>
+
+<TR>
+<TD>
+4. 
+</TD>
+
+<TD>
+HIRAYAMA
+</TD>
+
+<TD>
+Yuji 
+</TD>
+
+<TD>
+JPN 
+</TD>
+
+<TD>
+3. / 1
+</TD>
+
+<TD>
+80.00 
+</TD>
+
+<TD>
+36.30
+</TD>
+</TR>
+
+<TR>
+<TD>
+5. 
+</TD>
+
+<TD>
+PETIT
+</TD>
+
+<TD>
+Fran&ccedil;ois 
+</TD>
+
+<TD>
+FRA 
+</TD>
+
+<TD>
+2. / 1
+</TD>
+
+<TD>
+95.00 
+</TD>
+
+<TD>
+33.66
+</TD>
+</TR>
+
+<TR>
+<TD>
+6. 
+</TD>
+
+<TD>
+SARTORI
+</TD>
+
+<TD>
+Nicola 
+</TD>
+
+<TD>
+ITA 
+</TD>
+
+<TD>
+10. / 1
+</TD>
+
+<TD>
+49.00 
+</TD>
+
+<TD>
+31.02
+</TD>
+</TR>
+
+<TR>
+<TD>
+7. 
+</TD>
+
+<TD>
+MAZUER
+</TD>
+
+<TD>
+Fabien 
+</TD>
+
+<TD>
+FRA 
+</TD>
+
+<TD>
+11. / 1
+</TD>
+
+<TD>
+46.00 
+</TD>
+
+<TD>
+28.38
+</TD>
+</TR>
+
+<TR>
+<TD>
+8. 
+</TD>
+
+<TD>
+F&Uuml;RST
+</TD>
+
+<TD>
+Stefan 
+</TD>
+
+<TD>
+AUT 
+</TD>
+
+<TD>
+24. / 1
+</TD>
+
+<TD>
+22.00 
+</TD>
+
+<TD>
+0.00<A HREF="#before-1999"><b>(*)</b></a>
+</TD>
+</TR>
+
+<TR>
+<TD>
+9. 
+</TD>
+
+<TD>
+AROCENA
+</TD>
+
+<TD>
+Patxi 
+</TD>
+
+<TD>
+ESP 
+</TD>
+
+<TD>
+14. / 1
+</TD>
+
+<TD>
+39.00 
+</TD>
+
+<TD>
+0.00
+</TD>
+</TR>
+
+<TR>
+<TD>
+10. 
+</TD>
+
+<TD>
+SCASSA
+</TD>
+
+<TD>
+Severino 
+</TD>
+
+<TD>
+ITA 
+</TD>
+
+<TD>
+6. / 1
+</TD>
+
+<TD>
+62.00 
+</TD>
+
+<TD>
+0.00
+</TD>
+</TR>
+
+<TR>
+<TD>
+11. 
+</TD>
+
+<TD>
+GIUPPONI
+</TD>
+
+<TD>
+Luca 
+</TD>
+
+<TD>
+ITA 
+</TD>
+
+<TD>
+28. / 1
+</TD>
+
+<TD>
+18.00 
+</TD>
+
+<TD>
+0.00
+</TD>
+</TR>
+
+<TR>
+<TD>
+12. 
+</TD>
+
+<TD>
+ZARDINI
+</TD>
+
+<TD>
+Luca 
+</TD>
+
+<TD>
+ITA 
+</TD>
+
+<TD>
+4. / 1
+</TD>
+
+<TD>
+70.00 
+</TD>
+
+<TD>
+0.00
+</TD>
+</TR>
+
+<TR>
+<TD>
+13. 
+</TD>
+
+<TD>
+ALBRAND
+</TD>
+
+<TD>
+Vincent 
+</TD>
+
+<TD>
+FRA 
+</TD>
+
+<TD>
+16. / 1
+</TD>
+
+<TD>
+35.00 
+</TD>
+
+<TD>
+0.00
+</TD>
+</TR>
+
+<TR>
+<TD>
+14. 
+</TD>
+
+<TD>
+COROLLER
+</TD>
+
+<TD>
+Fr&eacute;d&eacute;ric 
+</TD>
+
+<TD>
+FRA 
+</TD>
+
+<TD>
+13. / 1
+</TD>
+
+<TD>
+41.00 
+</TD>
+
+<TD>
+0.00
+</TD>
+</TR>
+
+<TR>
+<TD>
+15. 
+</TD>
+
+<TD>
+SAMOILIN
+</TD>
+
+<TD>
+Pavel 
+</TD>
+
+<TD>
+RUS 
+</TD>
+
+<TD>
+5. / 1
+</TD>
+
+<TD>
+66.00 
+</TD>
+
+<TD>
+0.00
+</TD>
+</TR>
+
+<TR>
+<TD></TD>
+
+<TD></TD>
+
+<TD></TD>
+
+<TD></TD>
+
+<TD></TD>
+
+<TD>
+839.00 
+</TD>
+
+<TD></TD>
+</TR>
+
+<CAPTION ALIGN=BOTTOM>
+<P><B>fieldfaktor = 0.66 </B>= 839.00 / 1268.00</P>
+</CAPTION>
+</TABLE>
+
+<p><A NAME="before-1999"></A><B>(*)</b>
+<i>As the above competition was before 1999, only 50% of the field get points.
+<br>Nowerdays <u><b>all</b></u> competitors would get points.</i></p>
+
+<P>The second column 'rank' is the place of the competitor
+in the WR before this competition (after Black Sea '93). </P>
+
+<P>The next column 'ex.' shows the number of ex aquos in
+the WR on this place. </P>
+
+<P>This leads with the <A HREF="#cuwr-table">WR-table</A>
+to the points in the next column. All this points are added and the sum
+is divided by 1268, the maximum number of points (sum of the WR-table).
+</P>
+
+<P>The last colum shows the points a competitor gets for
+this competition. The field-factor read as a percentage is the same as
+the points for the first place.<BR>
+</P>
+
+<H2><A NAME="cuwr-table"></A><B><U>Appendix</U>: WR-table</B>
+</H2>
+
+<P>The column portion is the quotient of the points and the
+sum of all points (1268).</P>
+
+<TABLE BORDER=1 CELLPADDING=3 >
+	<tr>
+		<th>rank</th>
+		<th>points</th>
+		<th>portion %</th>
+		<th>% sum til that place</th>
+	</tr>
+<?php
+
+$cuwr_place2points = array(0,115,95,80,70,66,62,58,55,52,49,46,43,41,39,37,35,33,31,29,27,25,24,23,22,21,20,19,18,17,16);
+$total = array_sum($cuwr_place2points);
+
+foreach($cuwr_place2points as $place => $points)
+{
+	if (!$place) continue;
+	
+	$percentage = $points / $total;
+	$sum += $percentage;
+
+	echo "\t<tr align=\"center\">\n\t\t<td>$place.</td>\n\t\t<td>$points</td>\n";
+	echo "\t\t<td>".sprintf('%3.1f',100*$percentage)."%</td>\n\t\t<td>".sprintf('%3.1f',100*$sum)."%</td>\n";
+	echo "\t</tr>\n";
+}
+echo "\t<tr align=\"center\">\n\t\t<td><b>sum</b></td>\n\t\t<td>$total</td>\n\t</tr>";
+
+?>
+
+</TABLE>
+</TD>
+</TR>
+</TABLE>
+
+</BODY>
+</HTML>
