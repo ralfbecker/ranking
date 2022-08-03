64bdeadad8b365c77296e0b9b3c1e41db1c9a443
ralf <rb@egroupware.org>
2022-08-03 13:31:35 +0200

    Update ifsc footer

--- sitemgr/digitalrock/icc_footer.inc.php
+++ sitemgr/digitalrock/icc_footer.inc.php
@@ -3,10 +3,12 @@
 digital ROCK</A>, (c) 1990-<?php echo date('Y'); ?> by <?php echo mailto('RalfBecker@digitalROCK.de',
 'Ralf Becker'); ?>.</p>
 
-<p>Digital ROCK is contracted by
+<p>Digital ROCK was contracted in the past by
 <a href="https://www.ifsc-climbing.org" target="_blank">IFSC - International Federation of Sport Climbing</a>
 to provide services for athlete registration,<br/>
-on-site results-service and rankings for the various competition they run and to host their website.<br/>
+    on-site results-service and rankings for the various competition they run and to host their website.</p>
 
-If you are interessed in our services, please <a href="/kontakt.php">contact us</a>
+<p>Please visit <a href="https://www.ifsc-climbing.org" target="_blank">their website</a> for up-to-date information and rankings.</p>
+
+<p>If you are interested in our services, please <a href="/kontakt.php">contact us</a>
 or visit our <a href="/?load=kontakt.php%3Fprivacy-policy">privacy statement</a>.</p>
